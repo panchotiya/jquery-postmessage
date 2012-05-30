86b6dd810fea6018dda54d1cd28ca4d4787c0eed
Daniel Dotsenko <ddotsenko@willow-systems.com>
2012-05-29 18:07:34 -0700

    Removing hash-abuse and $.params() serialization
    IE 7 no longer allows it. Patched.
    $.params() turns objects into proprietary format. Modern browsers support passing objects around. We don't need mandatory serialization. It's like forbidding all people to use legs if some of us have lost ability to use legs.

--- jquery.ba-postmessage.js
+++ jquery.ba-postmessage.js
@@ -68,10 +68,7 @@
   // 
   // This method will call window.postMessage if available, setting the
   // targetOrigin parameter to the base of the target_url parameter for maximum
-  // security in browsers that support it. If window.postMessage is not available,
-  // the target window's location.hash will be used to pass the message. If an
-  // object is passed as the message param, it will be serialized into a string
-  // using the jQuery.param method.
+  // security in browsers that support it. 
   // 
   // Usage:
   // 
@@ -79,59 +76,55 @@
   // 
   // Arguments:
   // 
-  //  message - (String) A message to be passed to the other frame.
-  //  message - (Object) An object to be serialized into a params string, using
-  //    the jQuery.param method.
+  //  message - (Object) An object to be passed to the other window
   //  target_url - (String) The URL of the other frame this window is
-  //    attempting to communicate with. This must be the exact URL (including
-  //    any query string) of the other window for this script to work in
-  //    browsers that don't support window.postMessage.
+  //    attempting to communicate with. If given, this must be the exact URL
+  //    of the other window.
   //  target - (Object) A reference to the other frame this window is
   //    attempting to communicate with. If omitted, defaults to `parent`.
   // 
   // Returns:
   // 
-  //  Nothing.
+  //  True if messaging is supported and message has been (apparently) sent.
+  //  False otherwise
   
   $[postMessage] = function( message, target_url, target ) {
-    if ( !target_url ) { return; }
-    
-    // Serialize the message if not a string. Note that this is the only real
-    // jQuery dependency for this script. If removed, this script could be
-    // written as very basic JavaScript.
-    message = typeof message === 'string' ? message : $.param( message );
-    
-    // Default to parent if unspecified.
-    target = target || parent;
-    
     if ( has_postMessage ) {
+      // Default to parent if unspecified.
+      if (target === undef){
+        target = parent
+      }
+        
+      if (target_url === undef) {
+        // User explicitly chose not to care about domain of
+        // message recepient. Let's follow the command:
+        target_url = '*'
+      } else {
+        // Messaging domain matching happens on
+        //  prefix://fqdn:port 
+        // chopping off everything else.
+        target_url = target_url.replace( /([^:]+:\/\/[^\/]+).*/, '$1' )
+      }
+
       // The browser supports window.postMessage, so call it with a targetOrigin
       // set appropriately, based on the target_url parameter.
-      target[postMessage]( message, target_url.replace( /([^:]+:\/\/[^\/]+).*/, '$1' ) );
-      
-    } else if ( target_url ) {
-      // The browser does not support window.postMessage, so set the location
-      // of the target to target_url#message. A bit ugly, but it works! A cache
-      // bust parameter is added to ensure that repeat messages trigger the
-      // callback.
-      target.location = target_url.replace( /#.*$/, '' ) + '#' + (+new Date) + (cache_bust++) + '&' + message;
-    }
+      target[postMessage]( message, target_url );
+
+      return true
+    } 
+    return false
   };
   
   // Method: jQuery.receiveMessage
   // 
-  // Register a single callback for either a window.postMessage call, if
-  // supported, or if unsupported, for any change in the current window
-  // location.hash. If window.postMessage is supported and source_origin is
-  // specified, the source window will be checked against this for maximum
-  // security. If window.postMessage is unsupported, a polling loop will be
-  // started to watch for changes to the location.hash.
+  // Register a callback for a window.postMessage call. If window.postMessage
+  // is supported and source_origin is specified, the message source window 
+  // URL will be checked against this URL before. 
   // 
   // Note that for simplicity's sake, only a single callback can be registered
-  // at one time. Passing no params will unbind this event (or stop the polling
-  // loop), and calling this method a second time with another callback will
-  // unbind the event (or stop the polling loop) first, before binding the new
-  // callback.
+  // at one time. Passing no params will unbind this event 
+  // and calling this method a second time with another callback will
+  // unbind the event first, before binding the new callback.
   // 
   // Also note that if window.postMessage is available, the optional
   // source_origin param will be used to test the event.origin property. From
@@ -157,20 +150,17 @@
   //  source_origin - (Function) If window.postMessage is available and this
   //    function returns false when passed the event.origin property, the
   //    callback will not be called.
-  //  delay - (Number) An optional zero-or-greater delay in milliseconds at
-  //    which the polling loop will execute (for browser that don't support
-  //    window.postMessage). If omitted, defaults to 100.
   // 
   // Returns:
   // 
-  //  Nothing!
+  //  True if messaging is supported and message has been (apparently) sent.
+  //  False otherwise
   
-  $.receiveMessage = p_receiveMessage = function( callback, source_origin, delay ) {
+  $.receiveMessage = p_receiveMessage = function( callback, source_origin ) {
     if ( has_postMessage ) {
-      // Since the browser supports window.postMessage, the callback will be
-      // bound to the actual event associated with window.postMessage.
-      
       if ( callback ) {
+
+
         // Unbind an existing callback if it exists.
         rm_callback && p_receiveMessage();
         
@@ -178,43 +168,22 @@
         // unbinding.
         rm_callback = function(e) {
           if ( ( typeof source_origin === 'string' && e.origin !== source_origin )
-            || ( $.isFunction( source_origin ) && source_origin( e.origin ) === FALSE ) ) {
-            return FALSE;
+            || ( $.isFunction( source_origin ) && source_origin( e.origin ) === false ) ) {
+            return false;
           }
           callback( e );
         };
       }
       
       if ( window[addEventListener] ) {
-        window[ callback ? addEventListener : 'removeEventListener' ]( 'message', rm_callback, FALSE );
+        window[ callback ? addEventListener : 'removeEventListener' ]( 'message', rm_callback, false );
       } else {
         window[ callback ? 'attachEvent' : 'detachEvent' ]( 'onmessage', rm_callback );
       }
-      
-    } else {
-      // Since the browser sucks, a polling loop will be started, and the
-      // callback will be called whenever the location.hash changes.
-      
-      interval_id && clearInterval( interval_id );
-      interval_id = null;
-      
-      if ( callback ) {
-        delay = typeof source_origin === 'number'
-          ? source_origin
-          : typeof delay === 'number'
-            ? delay
-            : 100;
-        
-        interval_id = setInterval(function(){
-          var hash = document.location.hash,
-            re = /^#?\d+&/;
-          if ( hash !== last_hash && re.test( hash ) ) {
-            last_hash = hash;
-            callback({ data: hash.replace( re, '' ) });
-          }
-        }, delay );
-      }
+     
+      return true 
     }
+    return false
   };
   
 })(jQuery);
